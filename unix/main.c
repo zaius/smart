de947254ac929aa0892c60706656ba326258977d
David Kelso <david@kelso.id.au>
2005-03-03 16:09:11 +0000

    some intro work on the slip driver

--- unix/main.c
+++ unix/main.c
@@ -1,25 +1,31 @@
-// For opening files
-#include <fcntl.h>
-
 // Use the tun network device
 #include <sys/types.h>
 #include <net/if_tun.h>
 
-// printf
-#include <stdio.h>
-
-// write
-#include <unistd.h>
-
-#include <poll.h>
+#include <fcntl.h>		// open
+#include <stdio.h>		// printf
+#include <stdlib.h>		// malloc
+#include <unistd.h>		// write
+#include <poll.h>		// poll
+#include <signal.h>		// signals
 
 // Local includes
+#include "main.h"
 #include "slip.h"
+
+volatile sig_atomic_t quit = FALSE;
+void sigterm(int signal) {
+	quit = TRUE;
+}
 
 int main(int argc, char **argv) {
     int tunnel, serial;
-    struct pollfd * fds[2];
+    struct pollfd fds[2];
 
+	// Set up signal handlers
+	signal(SIGINT, sigterm);
+
+	// Open the tunnel interface
     tunnel = open("/dev/tun0", 0, 0);
 
     if (!tunnel) {
@@ -27,18 +33,49 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    while (1) {
-    
-    }
+	// Put the details of the tunnel interface into the fd array for polling
+	fds[0].fd = tunnel;
+	fds[0].events = POLLIN;
+	fds[0].revents = POLLIN;
 
-    serial = open("/dev/cua00", 0, 0);
+	// Open the serial interface
+    serial = open("/dev/cuaa0", 0, 0);
 
     if (!serial) {
         printf("Error opening serial");
         return 1;
     }
+	
+	// Put the details of the tunnel interface into the fd array for polling
+	fds[1].fd = serial;
+	fds[1].events = POLLIN;
+	fds[1].revents = POLLIN;
     
-    write(tunnel, "hello", 5);
 
-    return 0;
+    // write(tunnel, "hello", 5);
+
+	// Start the main loop
+	while (quit == FALSE) {
+		int result = 0;
+		printf("hi\n");
+		result = poll(fds, 2, 500);
+		
+		// timed out, continue the loop
+		if (result == 0) continue;
+		// error in poll, exit
+		if (result < 0) return -1;
+		
+		if (result == serial) {
+			printf("serial\n");
+		}
+		else {
+			printf("Network\n");
+		}
+		
+	}
+	
+	// We must have received a signal, tidy up and exit
+	printf("Exiting...\n");
+
+	return 0;
 }
