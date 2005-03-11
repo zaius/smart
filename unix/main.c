0cf3ad522d586bd37d96c6a0e99dc8a756740142
David Kelso <david@kelso.id.au>
2005-03-11 06:25:51 +0000

    trying to get poll working

--- unix/main.c
+++ unix/main.c
@@ -19,19 +19,20 @@ void sigterm(int signal) {
 }
 
 int main(int argc, char **argv) {
-    int tunnel, serial;
-    struct pollfd fds[2];
+	int tunnel, serial, i;
+	struct pollfd fds[2];
+	char buffer[255];
 
 	// Set up signal handlers
 	signal(SIGINT, sigterm);
 
 	// Open the tunnel interface
-    tunnel = open("/dev/tun0", 0, 0);
+	tunnel = open("/dev/tun0", 0, 0);
 
-    if (!tunnel) {
-        printf("Error opening tunnel");
-        return 1;
-    }
+	if (!tunnel) {
+		printf("Error opening tunnel");
+		return 1;
+	}
 
 	// Put the details of the tunnel interface into the fd array for polling
 	fds[0].fd = tunnel;
@@ -39,12 +40,12 @@ int main(int argc, char **argv) {
 	fds[0].revents = POLLIN;
 
 	// Open the serial interface
-    serial = open("/dev/cuaa0", 0, 0);
+	serial = open("/dev/cuaa0", 0, 0);
 
-    if (!serial) {
-        printf("Error opening serial");
-        return 1;
-    }
+	if (!serial) {
+		printf("Error opening serial");
+		return 1;
+	}
 	
 	// Put the details of the tunnel interface into the fd array for polling
 	fds[1].fd = serial;
@@ -52,24 +53,34 @@ int main(int argc, char **argv) {
 	fds[1].revents = POLLIN;
     
 
-    // write(tunnel, "hello", 5);
+	// write(tunnel, "hello", 5);
 
 	// Start the main loop
 	while (quit == FALSE) {
 		int result = 0;
-		printf("hi\n");
 		result = poll(fds, 2, 500);
 		
 		// timed out, continue the loop
-		if (result == 0) continue;
-		// error in poll, exit
-		if (result < 0) return -1;
+		if (result == 0) {
+			printf("hi\n");
+			continue;
+		}
 		
 		if (result == serial) {
 			printf("serial\n");
+			read(serial, buffer, 255);
+
+			for (i = 0; i < 255; i++) {
+				printf("%c", buffer[i]);
+			}
+			printf("\n");
+		}
+		else if (result == tunnel) {
+			printf("Network\n");
 		}
 		else {
-			printf("Network\n");
+			// error in poll, exit
+			return -1;
 		}
 		
 	}
