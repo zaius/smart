f39a2fe4752d9ff8ebd96a9f9b137dd6b2643e44
David Kelso <david@kelso.id.au>
2005-03-23 12:50:49 +0000

    move some more variables into defines

--- unix/main.c
+++ unix/main.c
@@ -22,12 +22,17 @@
 // stuff for inet_addr
 #include <netinet/in.h>
 #include <arpa/inet.h>
-		  
+
 
 // Amount to read on a serial read
 #define S_READ 255
 #define SERIAL_DEV "/dev/cuaa0"
 #define TUNNEL_DEV "/dev/tun2"
+#define TUNNEL_IF "tun2"
+
+#define IP_ADDR "10.0.0.1"
+#define BROAD_ADDR "10.0.0.255"
+#define NET_MASK "255.255.255.0"
 
 #define TUNNEL_INDEX 0
 #define SERIAL_INDEX 1
@@ -56,17 +61,15 @@ int main(int argc, char **argv) {
 	if (serial < 0) {
 		err(serial, "Error opening serial");
 	}
-	
+
 	// Put the details of the serial interface into the fd array for polling
 	fds[SERIAL_INDEX].fd = serial;
 	fds[SERIAL_INDEX].events = POLLIN;
-	
+
 	// Open the tunnel interface
 	tunnel = open(TUNNEL_DEV, O_RDWR);
 
-	if (tunnel < 0) {
-		err(tunnel, "Error opening tunnel");
-	}
+	if (tunnel < 0) err(tunnel, "Error opening tunnel");
 
 	tunnelinfo.baudrate = 9600;		// linespeed 
 	tunnelinfo.mtu = 1500;     		// maximum transmission unit 
@@ -91,27 +94,27 @@ int main(int argc, char **argv) {
 	// create a socket in order to configure the interface
 	sock = socket(AF_INET, SOCK_RAW, 0);
 	if (sock == -1)	warn("opening socket");	
-	
+
 	// Set the name of the interface we're referring to
-	strlcpy(ifa.ifra_name, "tun2", IFNAMSIZ);
+	strlcpy(ifa.ifra_name, TUNNEL_IF, IFNAMSIZ);
 
 	// Set the IP Address
 	in = (struct sockaddr_in *)&ifa.ifra_addr;
 	in->sin_family = AF_INET;
 	in->sin_len = sizeof(ifa.ifra_addr);
-	in->sin_addr.s_addr = inet_addr("10.0.0.1");
+	in->sin_addr.s_addr = inet_addr(IP_ADDR);
 
 	// Set the Broadcast Address
 	in = (struct sockaddr_in *)&ifa.ifra_broadaddr;
 	in->sin_family = AF_INET;
 	in->sin_len = sizeof(ifa.ifra_addr);
-	in->sin_addr.s_addr = inet_addr("10.0.0.255");
+	in->sin_addr.s_addr = inet_addr(BROAD_ADDR);
 
 	// Set the net mask
 	in = (struct sockaddr_in *)&ifa.ifra_mask;
 	in->sin_family = AF_INET;
 	in->sin_len = sizeof(ifa.ifra_addr);
-	in->sin_addr.s_addr = inet_addr("255.255.255.0");
+	in->sin_addr.s_addr = inet_addr(NET_MASK);
 
 	// Assign the addresses to the interface
 	i = ioctl(sock, SIOCAIFADDR, &ifa);
@@ -128,9 +131,9 @@ int main(int argc, char **argv) {
 	// Start the main loop
 	while (quit == FALSE) {
 		int result = 0;
-		
+
 		result = poll(fds, 2, 500);
-		
+
 		if (result < 0) {
 			// error in poll, warn
 			warn("Poll Error");
@@ -147,10 +150,10 @@ int main(int argc, char **argv) {
 			int length;
 
 			printf("serial\n");
-			
+
 			do {
 				length = read(serial, buffer, S_READ);
-			
+
 				for (i = 0; i < length; i++) {
 					printf("0x%02x ", buffer[i]);
 				}	
@@ -158,15 +161,15 @@ int main(int argc, char **argv) {
 
 			printf("\n");
 		}
-		
+
 		if (fds[TUNNEL_INDEX].revents != 0) {
 			int length; 
-			
+
 			printf("Network\n");
 
 			do {
 				length = read(tunnel, buffer, S_READ);
-			
+
 				for (i = 0; i < length; i++) {
 					printf("0x%02x ", buffer[i]);
 				}	
@@ -175,7 +178,7 @@ int main(int argc, char **argv) {
 			printf("\n");
 		}
 	}
-	
+
 	// We must have received a signal, tidy up and exit
 	printf("Exiting...\n");
 	close(serial);
